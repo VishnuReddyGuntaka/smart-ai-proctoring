ed8e72b5c6668cdcb92e75aff58bbab876f7b7a1
Vishnu Reddy <iamvishnureddy424@gmail.com>
2026-06-11 12:52:44 +0530

    Add model and detection configuration settings

---
+++ config.py
@@ -0,0 +1,17 @@
+# Model Configuration
+MODEL_CFG = 'model/model.cfg'
+MODEL_WEIGHTS = 'model/model.weights'
+MODEL_LABELS = 'model/modellabels'
+HAAR_CASCADE = 'model/haarcascade_frontalface_default.xml'
+CNN_MODEL_JSON = 'model/cnnmodel.json'
+CNN_MODEL_WEIGHTS = 'model/cnnmodel_weights.h5'
+TRAINING_X = 'model/X.txt.npy'
+TRAINING_Y = 'model/Y.txt.npy'
+
+# Detection Settings
+CONFIDENCE_THRESHOLD = 0.5
+NMS_THRESHOLD = 0.3
+
+# Window Settings
+WINDOW_TITLE = 'Smart AI Proctoring System'
+WINDOW_SIZE = '1300x1200'
